71ce0164d2e181f004210485fbb5a680e5306e05
victorya <89627876028x@gmail.com>
2021-09-06 11:49:51 +0700

    Add uroven0

---
+++ src/uroven0/zadanie5.java
@@ -0,0 +1,25 @@
+/*
+Объявляем переменные
+Напиши программу, которая в методе main объявляет такие переменные:
+name типа String, age типа int и city типа String.
+
+Требования:
+1. Объяви переменную name типа String.
+2. Объяви переменную age типа int.
+3. Объяви переменную city типа String.
+4. Должны быть объявлены 3 переменные.
+
+ */
+package uroven0;
+
+/**
+ *
+ * @author Титаренко В.А.
+ */
+public class zadanie5 {
+    public static void main(String[] args) {
+        String name = "Victoriy";
+        int age = 20;
+        String city = "Tomsk";
+        System.out.println("Имя: " + name + '\n' + "возраст " + age + '\n' + "city " + city );  }   
+}
---
+++ src/uroven0/zadanie8.java
@@ -0,0 +1,41 @@
+/*
+ * 2 + 3 = 5
+Закомментируй несколько строк, чтобы на экран вывелась надпись:
+2 плюс 3 равно 5
+Примечание: комментировать строки с объявлением переменных нельзя.
+
+Требования:
+1. Программа должна выводить на экран надпись "2 плюс 3 равно 5"
+2. Нельзя изменять строки с объявлением переменных.
+3. Нужно закомментировать минимум одну строку.
+4. Нельзя изменять или добавлять команды, отвечающие за вывод.
+
+ */
+package uroven0;
+
+
+/**
+ *
+ * @author Викуся
+ */
+public class zadanie8 {
+     public static void main(String[] args) {
+        String s = "23";
+        int a = 3, b = 2;
+        String four = "четыре";
+        String three = "3";
+
+       // System.out.print("два");
+       // System.out.print(" плюс ");
+        //System.out.print(s);
+        System.out.print(b);
+        System.out.print(" плюс ");
+        //System.out.print("три");
+        System.out.print(a);
+        System.out.print(" равно ");
+        //System.out.print("пять");
+        System.out.print(a + b);
+    }
+
+    
+}
