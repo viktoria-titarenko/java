6aabfa875f40f6f02a952f0a897e09efc10cd659
victorya <89627876028x@gmail.com>
2021-10-05 12:26:34 +0700

    add lab 5

---
+++ src/lab5/zadanie1.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+import java.util.Scanner;
+/**
+ *
+ * @author Викуся
+ */
+public class zadanie1 {
+    public static void main(String[] args){
+    System.out.println("vvedi chislo");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        switch(number){
+            case(0):
+                System.out.println("zero");
+                break;
+            case(1):
+                System.out.println("one");
+                break;
+            case(2):
+                System.out.println("two");
+                break;
+            case(3):
+                System.out.println("thre");
+                break;
+            case(4):
+                System.out.println("four");
+                break;
+            case(5):
+                System.out.println("five");
+                break;
+            case(6):
+                System.out.println("six");
+                break;
+            case(7):
+                System.out.println("seven");
+                break;
+            case(8):
+                System.out.println("eight");
+                break;
+            case(9):
+                System.out.println("nine");
+                break;
+        }
+                
+                
+        } 
+    }
+    
+
